77c5ecc427b11f916358c034a0298d2abf2b5065
Chris Mountford <Chris Mountford>
2017-04-22 16:13:54 +0100

    Main window started

---
+++ runApp.py
@@ -0,0 +1,8 @@
+import trackerGUI
+from tkinter import *
+
+if __name__ == "__main__":
+    root = Tk()
+    app = trackerGUI.MainWindow(main=root)
+    root.mainloop()
+
--- trackerGUI.py
+++ trackerGUI.py
@@ -1,7 +1,24 @@
 try:
-    from tkinter import *
+    import tkinter as tk
 
 except ImportError:
-    print("Built using tkinter with Python 3")
-    sys.exit()
+    raise ImportError("Built using tkinter with Python 3")
+
+
+class MainWindow(tk.Frame):
+    def __init__(self, main):
+        tk.Frame.__init__(self, main)
+        self.main = main
+
+        self.init_main_win()
+
+    def init_main_win(self):
+        self.main.title("CM's Progress Tracker")
+        self.main.geometry("1000x750")
+
+
+
+
+
+
 
